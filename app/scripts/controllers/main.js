5e2b8e833902d09664e41061e155b9950ca6fe98
fakeco <fakeco@mailinator.com>
2015-06-21 17:58:39 -0400

    code clean up

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -11,10 +11,8 @@ angular.module('challengeApp')
 	.controller('MainCtrl', function($scope, $interval, ScheduleService) {
 		var self = this;
 		self.time = null;
-		self.startTimes = [];
-		self.currentMeeting = null;
 		self.currentTime = function() {
-			this.time = new Date();
+			self.time = new Date();
 		};
 
 		ScheduleService.getSchedule().then(function(response) {
